de927362a2aa42d9078f64ad6eebe41cfaab0713
Simon Lynch <simon.lynch@hashicorp.com>
2025-03-24 10:40:19 +1100

    Update provider versions in Terraform configuration for AWS and random providers

--- deploy_secure_ai_aws/vault_aws_auth/providers.tf
+++ deploy_secure_ai_aws/vault_aws_auth/providers.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     random = {
       source = "hashicorp/random"
-      version = "~> 3.6.0"
+      version = "~> 3.0"
     }
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 3.15"
+      version = ">= 5.0"
     }
   }
   required_version = ">= 1.8"
