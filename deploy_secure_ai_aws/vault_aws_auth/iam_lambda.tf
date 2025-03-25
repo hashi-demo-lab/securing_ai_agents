c87d32aab2ba6cf48b21200f1f61d2da42acefcc
glennchia-hashi <glenn.chia@hashicorp.com>
2025-03-25 15:44:00 +0800

    chore: rm permissions from lambda
    Co-authored-by: GlennChia <glennchia7@gmail.com>

--- deploy_secure_ai_aws/vault_aws_auth/iam_lambda.tf
+++ deploy_secure_ai_aws/vault_aws_auth/iam_lambda.tf
@@ -1,13 +1,7 @@
 // Create IAM role for Lambda with Vault authentication
 resource "aws_iam_role" "lambda" {
   name               = "${var.aws_environment_name}-lambda-role"
-  assume_role_policy = var.assume_role ? data.aws_iam_policy_document.assume_role_lambda_plus_root[0].json : data.aws_iam_policy_document.assume_role_lambda.json
-}
-
-resource "aws_iam_role_policy" "lambda" {
-  name   = "${var.aws_environment_name}-lambda-policy"
-  role   = aws_iam_role.lambda.id
-  policy = var.assume_role ? data.aws_iam_policy_document.lambda_plus_assume_role[0].json : data.aws_iam_policy_document.lambda.json
+  assume_role_policy = data.aws_iam_policy_document.assume_role_lambda.json
 }
 
 data "aws_iam_policy_document" "assume_role_lambda" {
@@ -22,24 +16,6 @@ data "aws_iam_policy_document" "assume_role_lambda" {
   }
 }
 
-data "aws_iam_policy_document" "assume_role_lambda_plus_root" {
-  count = var.assume_role ? 1 : 0
-
-  statement {
-    effect  = "Allow"
-    actions = ["sts:AssumeRole"]
-
-    principals {
-      type        = "Service"
-      identifiers = ["lambda.amazonaws.com"]
-    }
-
-    principals {
-      type        = "AWS"
-      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"]
-    }
-  }
-}
 ### THIS IS NOT RECOMMENDED FOR PRODUCTION USE ###
 ## workaround for Doormat ## Vault
 data "aws_region" "current" {}
@@ -49,49 +25,6 @@ data "aws_caller_identity" "current" {}
 data "aws_iam_policy" "demo_user_permissions_boundary" {
   name = "DemoUser"
 }
-
-data "aws_iam_policy" "security_compute_access" {
-  name = "SecurityComputeAccess"
-}
-
-data "aws_iam_policy_document" "lambda" {
-  statement {
-    sid    = "LambdaLogs"
-    effect = "Allow"
-
-    actions = [
-      "logs:CreateLogGroup",
-      "logs:CreateLogStream",
-      "logs:PutLogEvents"
-    ]
-
-    resources = ["*"]
-  }
-}
-
-data "aws_iam_policy_document" "lambda_plus_assume_role" {
-  count = var.assume_role ? 1 : 0
-
-  statement {
-    sid    = "LambdaLogs"
-    effect = "Allow"
-
-    actions = [
-      "logs:CreateLogGroup",
-      "logs:CreateLogStream",
-      "logs:PutLogEvents"
-    ]
-
-    resources = ["*"]
-  }
-
-  statement {
-    effect    = "Allow"
-    actions   = ["sts:AssumeRole"]
-    resources = ["*"]
-  }
-}
-
 
 #### Vault
 
