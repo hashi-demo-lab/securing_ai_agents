0d06a6d0c0d369c7e64f773a827762cd5fa886c8
Simon Lynch <simon.lynch@hashicorp.com>
2025-03-27 11:18:12 +1100

    refactor: remove vso_status and gitops_status outputs from outputs.tf

--- deploy_secure_ai_openshift/config_openshift_vso_gitops/outputs.tf
+++ deploy_secure_ai_openshift/config_openshift_vso_gitops/outputs.tf
@@ -8,16 +8,6 @@ output "gitops_namespace" {
   value       = try(kubernetes_namespace.gitops_operator[0].metadata[0].name, null)
 }
 
-output "vso_status" {
-  description = "Status of Vault Secrets Operator installation"
-  value       = try(kubernetes_manifest.vso[0].manifest.status, "not installed")
-}
-
-output "gitops_status" {
-  description = "Status of OpenShift GitOps Operator installation"
-  value       = try(kubernetes_manifest.gitops_operator[0].manifest.status, "not installed")
-}
-
 output "vso_operator_manifest" {
   description = "Applied Vault Secrets Operator manifest"
   value       = try(kubernetes_manifest.vso[0].manifest, null)
