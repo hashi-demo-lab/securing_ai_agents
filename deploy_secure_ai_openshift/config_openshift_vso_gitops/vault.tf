91cee0efacf47f84fa5950e4afc38fb0f70aaf89
Simon Lynch <simon.lynch@hashicorp.com>
2025-03-31 15:33:34 +1100

    feat: parameterize Vault JWT auth backend role for dynamic namespace support

--- deploy_secure_ai_openshift/config_openshift_vso_gitops/vault.tf
+++ deploy_secure_ai_openshift/config_openshift_vso_gitops/vault.tf
@@ -7,20 +7,20 @@
 
 resource "vault_jwt_auth_backend_role" "app1_role" {
   backend                 = var.k8s_jwt_auth_backend
-  role_name               = "app1"
+  role_name               = "${var.app_namespace}_role"
   role_type               = "jwt"
-  bound_audiences         = ["https://kubernetes.default.svc.cluster.local"]
+  bound_audiences         = ["${var.k8s_bound_audience}"]
   user_claim              = "/kubernetes.io/serviceaccount/name"
   user_claim_json_pointer = true
-  token_policies          = ["access-all"]
+  token_policies          = ["${var.app_namespace}_role"]
 
   # Allows wildcards or regex in bound_claims
   bound_claims_type = "glob"
 
   bound_claims = {
     # Match any service account starting with "app1-service-"
-    "/kubernetes.io/serviceaccount/name" = "app1-service-*"
+    "/kubernetes.io/serviceaccount/name" = "${var.app_namespace}-service-*"
     # Restrict to the "app1" namespace
-    "/kubernetes.io/namespace" = "app1"
+    "/kubernetes.io/namespace" = "${var.app_namespace}"
   }
 }
